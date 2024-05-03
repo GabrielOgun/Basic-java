7c2aef84036e15e51dc9734e331b9bed959a2522
Gabriel Marques <gahmarks@hotmail.com>
2024-05-03 19:37:20 -0300

    terminando operadores adicionando metodos

---
+++ Anotaçoes/src/dio/sintaxe/metodos/metodos.java
@@ -0,0 +1,20 @@
+package dio.sintaxe.metodos;
+
+public class metodos {
+
+    public static void main (String[] args){
+        /*criterios de criaçao de metodos (boas praticas):
+        nomeado como verbo 
+        seguir o padrao camelCase primeira em minusculo segunda em maisculo
+        nome claro e objetivo
+        */
+
+        /*nao existe em java o conceito de metodos globais
+        todos os metodos sao definidos em class
+        */
+
+        
+
+    }
+    
+}
